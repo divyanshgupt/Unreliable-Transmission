Correct normalised convolution and stacked computation

SuperSpike/functions/run_single_neuron.py
@@ -71,7 +71,7 @@ def run_single_neuron(input_trains, weights, target, args):
   spk_rec = torch.stack(spk_rec, dim=0)
   spk_rec = torch.flatten(spk_rec) # stack as a 1-D array for easy difference with target train for error signal evaluation
   # compute presynaptic traces of shape: (nb_inputs, timesteps) 
-  presynaptic_traces = functions.presynaptic_trace(input_trains, args) 
+  presynaptic_traces = functions.new_presynaptic_trace(input_trains, args) 
 
   # evaluate hebbian coincidence
   h = mem_rec.T - thres  # shape: (nb_outputs, timesteps)
@@ -84,6 +84,6 @@ def run_single_neuron(input_trains, weights, target, args):
   eligibility = functions.eligibility_trace(hebbian, args)
   
   # error signal
-  error = functions.error_signal(spk_rec, target, args)
+  error = functions.new_error_signal(spk_rec, target, args)
 
   return mem_rec, spk_rec, error, eligibility, presynaptic_traces

SuperSpike/functions/train_single_neuron.py
@@ -1,7 +1,7 @@
 import numpy as np
 import torch
 from matplotlib import pyplot as plt
-import tqdm
+from tqdm import tqdm
 import functions
 import pickle
 import datetime
@@ -44,7 +44,49 @@ def train_single_neuron(input_trains, target, weights, r_0, args):
     v_ij_rec = torch.zeros((nb_inputs, nb_outputs, nb_epochs), device=device, dtype=dtype)
     r_ij_rec = torch.zeros((nb_inputs, nb_outputs, nb_epochs), device=device, dtype=dtype)
 
-    for i in range(nb_epochs):
+
+    for i in tqdm(range(nb_epochs)):
+        
+        mem_rec, spk_rec, error_rec, eligibility_rec, pre_trace_rec = functions.run_single_neuron(input_trains, weights,
+                                                                                        target, args)
+        loss = functions.new_van_rossum_loss(spk_rec, target, args)
+        loss_rec[i] = loss
+
+        # Weight update
+        weight_updates = torch.sum(error_rec * eligibility_rec, dim=2)
+        assert weight_updates.shape == (nb_inputs, nb_outputs), "wegiht_updates shape incorrect"
+
+        # per-parameter learning rate
+    #   g_ij2 = (error_rec * eligibility_rec)[:, :, -1]**2 # this has a problem 
+        g_ij2 = torch.sum((error_rec*eligibility_rec)**2, dim=2)
+        assert g_ij2.shape == (nb_inputs, nb_outputs), "g_ij2 shape incorrect"
+        # Question 1: Whether to take the value of g_ij at the last timestep in each epoch or to take the sum of its values over all timesteps in the epoch?
+        # Question 2: How to do normalized convolution for error_signal and eligibility_trace?
+
+        v_ij, _ = torch.max(torch.stack([gamma*v_ij, g_ij2], dim=2), dim=2) # shape: (nb_inputs, nb_outputs)
+        assert v_ij.shape == (nb_inputs, nb_outputs), "v_ij shape incorrect"
+        
+        # Evaluate learning rate for this epoch
+        r_ij = r_0 / torch.sqrt(v_ij)   # shape: (nb_inputs, nb_outputs)
+        # Store learning rate information for this epoch for xth weight
+        g_ij2_rec[:, :, i] = g_ij2 
+        v_ij_rec[:, :, i] = v_ij
+        r_ij_rec[:, :, i] = r_ij
+
+     #   rate_med = torch.median(r_ij)
+     #   print("Median Learning Rate:", rate_med)
+     #   rate_mean = torch.mean(r_ij)
+     #   print("Avg. Learning Rate:", rate_mean)
+
+        weights += r_ij * weight_updates
+
+    
+    learning_rate_params = (r_ij_rec, v_ij_rec, g_ij2_rec)
+
+    return weights, loss_rec, learning_rate_params
+        
+    """
+    for i in tqdm(range(nb_epochs)):
 
         #print("\n Iteration:", i+1)
         mem_rec, spk_rec, error_rec, eligibility_rec, pre_trace_rec = functions.run_single_neuron(input_trains, weights,
@@ -96,6 +138,4 @@ def train_single_neuron(input_trains, target, weights, r_0, args):
 
         weights += r_ij * weight_updates
 
-    learning_rate_params = (r_ij_rec, v_ij_rec, g_ij2_rec)
-
-    return weights, loss_rec, learning_rate_params
+    """

SuperSpike/main_single_neuron.py
@@ -7,7 +7,7 @@ import functions
 #import functions.plot
 import datetime
 import json # To save simulation parameters in a text file
-import pdb
+#import pdb
 import pickle
 
 # set device
@@ -37,7 +37,7 @@ args = {'thres': -50,
         'nb_outputs': 1,
         'device': device, # for functions in different modules
         'dtype': dtype,
-        'nb_epochs': 400
+        'nb_epochs': 1000
         } 
 
 
@@ -68,16 +68,21 @@ target[:: nb_steps//5] = 1
 weights = functions.initialize_weights(nb_inputs, nb_outputs, args, scale=80) # initialize weights
 
 # v_ij = 1e-2*torch.zeros((nb_inputs, nb_outputs), device=device, dtype=dtype)
-learning_rates = np.array([10, 5, 1, 0.5, 0.1]) * 1e-3
+#learning_rates = np.array([10, 5, 1, 0.5, 0.1]) * 1e-3
+
+learning_rates = np.array([5, 1, 10, 0.5, 0.1]) * 1e-3
+#learning_rates = learning_rates[::-1]
 
 for r_0 in learning_rates:
     #r_0 = 5e-3 # basal learning rate
-
+    print("Learning rate =", r_0)
     new_weights, loss_rec, learning_rate_params = functions.train_single_neuron(input_trains, target, weights, r_0, args)
     #r_ij, v_ij, g_ij2 = learning_rate_params
-    #plt.plot(loss_rec)
-    #plt.title("Loss")
-    #plt.show()
+    plt.plot(loss_rec[1:])
+    plt.title("Loss, learning-rate = " + str(r_0))
+    plt.show()
+
+    """
 
     data_folder = "data/" + str(datetime.datetime.today())[:10] + ' rate = ' + str(r_0) + '/'
     #os.makedirs(location)
@@ -92,6 +97,7 @@ for r_0 in learning_rates:
     #location = ''
     #location = os.path.cudir()
     # Store parameters (in dict args) in the given location
+    
     functions.plot.plot_loss(loss_rec, location, args) # Saves the loss-over-epochs plot in the given location
     functions.plot.plot_learning_rate_params(learning_rate_params, location, args)
 
@@ -116,7 +122,7 @@ for r_0 in learning_rates:
    # json.dump(args, args_file)
     args_file = open(file_name, 'a')
     args_file.write(str(args))
-
+    """
 
 """plt.plot(loss_rec)
 plt.title("Loss over epochs")