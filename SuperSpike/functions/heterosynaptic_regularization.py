f71c527b39b6e257649e1000d9256da94b13e325
Divyansh Gupta <divyansh.gupta@students.iiserpune.ac.in>
2022-02-02 21:54:29 +0530

    implemented hetersynaptic regularization for multilayer network with random feedback

---
+++ SuperSpike/functions/heterosynaptic_regularization.py
@@ -0,0 +1,39 @@
+import torch
+import numpy as np
+from matplotlib import pyplot as plt
+from tqdm import tqdm
+import functions
+
+def heterosynaptic_regularization(activity, args):
+    """
+    Evaluates the heterosynaptic regularization term by computing the 
+    exponential moving average of the instantaeous post-synaptic firing rate
+    
+    Returns:
+        regularization - shape:(nb_)
+    """
+
+    device = args['device']
+    dtype = args['dtype']
+    nb_steps = args['nb_steps']
+    dt = args['timestep_size']
+    tau_het = args['tau_het']
+    rho = args['rho'] # regularization strength
+
+    nb_outputs = activity.shape[0]
+
+    regularization = torch.zeros((nb_outputs, nb_steps), device=device, dtype=dtype)
+
+    time_array = torch.arange(0, nb_steps*dt, dt, device=device, dtype=dtype)
+    model_trace = torch.exp(-time_array/tau_het).to(device)
+
+    for t in range(nb_steps):
+        indices = activity >= 1
+        regularization[indices, t:] += model_trace[nb_steps - t]
+
+    regularization = torch.sum(regularization, dim=1)*dt # simple first order Euler integration method
+
+    assert regularization.shape == (nb_outputs,), "Regularization term shape incorrect"
+
+    return regularization
+
